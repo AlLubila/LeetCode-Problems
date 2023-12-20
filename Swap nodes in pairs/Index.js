924b5e6dff5ec8bb12d4370a4ee59a29e528b177
Albi <allubila016@gmail.com>
2023-12-20 14:31:51 +0100

    ongoing

--- Swap nodes in pairs/Index.js
+++ Swap nodes in pairs/Index.js
@@ -29,17 +29,23 @@ const array1 = [1, 2, 3, 4];
 const array2 = [];
 const array3 = [1];
 
-let swapPairs = function (head) {
+let swapPairs = (head) => {
   // first case
   if (head.length % 2 == 0) {
+    let normalOrder = [array1[0], array1[1], array1[2], array1[3]];
+    let swapped = [array1[1], array1[0], array1[3], array1[2]];
+    return swapped;
   }
+
   // second case
   else if (head.length == 0) {
     return head;
   }
+
   // Third case
   else head.length == 1;
   {
     return head;
   }
 };
+console.log(swapPairs(array2));
